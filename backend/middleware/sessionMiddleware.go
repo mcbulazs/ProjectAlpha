d3453e49ebf683f44efa9df34f11248ed5804f39
Bulazs <mcbulazslol@gmail.com>
2024-01-24 20:56:00 +0100

    fixed /auth endpoint

--- backend/middleware/sessionMiddleware.go
+++ backend/middleware/sessionMiddleware.go
@@ -8,7 +8,7 @@ import (
 
 func SessionMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Get the session from the store
+		//setting up header
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
@@ -16,14 +16,20 @@ func SessionMiddleware(next http.Handler) http.Handler {
 		if slices.Contains(functions.AllowedOrigins, origin) {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
-
-		if r.URL.Path != "/login" && r.URL.Path != "/register" {
+		//except for login and register u need session for everything
+		unSessionedEndPoints := []string{"/login", "/register"}
+		if !slices.Contains(unSessionedEndPoints, r.URL.Path) {
+			// Get the session from the store
 			session, _ := functions.Store.Get(r, "session")
 
-			_, ok := session.Values["user_id"].(int)
-			if !ok {
+			_, ok := session.Values["user_id"].(int) // valid user
+			if !ok && r.URL.Path != "/auth" {
 				http.Error(w, "User not authenticated", http.StatusUnauthorized)
 				return
+			} else if !ok && r.URL.Path == "/auth" { //just to decide whetger the user have a session or not
+				w.Header().Set("authenticated", "false")
+			} else if ok && r.URL.Path == "/auth" {
+				w.Header().Set("authenticated", "true")
 			}
 		}
 		next.ServeHTTP(w, r)
